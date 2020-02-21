d7695cff4b658e089940440d93f4985fde6d52bd
Nicola Pisanti <nicola@npisanti.com>
2020-02-21 18:40:54 +0100

    stereo soft clip

---
+++ src/dynamics/StereoSoftClip.cpp
@@ -0,0 +1,44 @@
+
+// SoftClip.cpp
+// Nicola Pisanti, MIT License, 2019
+
+#include "StereoSoftClip.h"
+
+void np::dynamics::StereoSoftClip::patch(){
+
+    addModuleInput( "signal", inputGain );
+    addModuleOutput( "signal", outputGain );
+
+    clip0.setOversampleLevel(2);
+    clip1.setOversampleLevel(2);
+    
+    chan0.input >> inputGain.ch(0) >> upsampler0 >> clip0 >> downsampler0 >> outputGain.ch(0) >> chan0.output;
+    
+    chan0.input >> inputGain.ch(1) >> upsampler1 >> clip1 >> downsampler1 >> outputGain.ch(1) >> chan1.output;
+    
+    clipThreshold >> clip0.in_threshold();
+    clipThreshold >> clip1.in_threshold();
+    
+    parameters.setName( "soft clip" );
+    parameters.add( inputGain.set("input gain", 0, -48, 36) );
+    parameters.add( clipThreshold.set("clip threshold", -3, 0, -12) );
+    parameters.add( outputGain.set("output gain", 0, -48, 24 ) );    
+}
+
+pdsp::Patchable & np::dynamics::StereoSoftClip::ch( int i ){
+    switch( i ){
+        default: 
+            std::cout<<"[pdsp] wrong index for StereoSoftClip ch, returning ch 0 \n"; 
+        case 0:
+            return chan0;
+            break;
+        case 1:
+            return chan1;
+            break;
+    }
+}
+
+ofParameterGroup & np::dynamics::StereoSoftClip::label( std::string name ){
+    parameters.setName(name);
+    return parameters;
+}
---
+++ src/dynamics/StereoSoftClip.h
@@ -0,0 +1,47 @@
+
+#pragma once
+
+#include "ofMain.h"
+
+#include "ofxPDSP.h"
+
+// SoftClip oversampled soft-clipper with adjustable threshold
+
+namespace np { namespace dynamics {
+    
+class StereoSoftClip : public pdsp::Patchable {
+
+public:    
+    StereoSoftClip() { patch(); }
+    StereoSoftClip( const StereoSoftClip & other ) { patch (); }
+
+    pdsp::Patchable & ch( int i );
+    
+    ofParameterGroup & label( std::string name );
+    
+    ofParameterGroup parameters;
+
+
+
+private:    
+
+
+    void patch();
+
+    pdsp::ChannelNode chan0;
+    pdsp::ChannelNode chan1;
+        
+    pdsp::SoftClip clip0;
+    pdsp::SoftClip clip1;
+    pdsp::IIRUpSampler2x upsampler0;
+    pdsp::IIRUpSampler2x upsampler1;
+    pdsp::IIRDownSampler2x downsampler0; 
+    pdsp::IIRDownSampler2x downsampler1; 
+    
+    pdsp::ParameterGain inputGain;
+    pdsp::Parameter     clipThreshold;
+    pdsp::ParameterGain outputGain;
+    
+};
+    
+}}
